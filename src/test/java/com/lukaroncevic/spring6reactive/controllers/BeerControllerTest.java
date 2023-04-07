7b88c50a6b26c5c0a562942e72f02c9c66f2d1ab
lroncevic <roncevicluka@outlook.com>
2023-04-07 18:21:52 +0200

    Added list beers test

---
+++ src/test/java/com/lukaroncevic/spring6reactive/controllers/BeerControllerTest.java
@@ -0,0 +1,26 @@
+package com.lukaroncevic.spring6reactive.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureWebTestClient
+@SpringBootTest
+class BeerControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void listBeers() {
+        webTestClient.get().uri(BeerController.BEER_PATH)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody().jsonPath("$.size()").isEqualTo(3);
+    }
+}
