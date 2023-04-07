9fed578ea91be8c2ec10db8f8d542b3c8b92d4f2
lroncevic <roncevicluka@outlook.com>
2023-04-07 21:51:44 +0200

    Test validation on create beer

--- src/test/java/com/lukaroncevic/spring6reactive/controllers/BeerControllerTest.java
+++ src/test/java/com/lukaroncevic/spring6reactive/controllers/BeerControllerTest.java
@@ -1,5 +1,6 @@
 package com.lukaroncevic.spring6reactive.controllers;
 
+import com.lukaroncevic.spring6reactive.domain.Beer;
 import com.lukaroncevic.spring6reactive.model.BeerDTO;
 
 import com.lukaroncevic.spring6reactive.repositories.BeerRepositoryTest;
@@ -38,6 +39,17 @@ class BeerControllerTest {
                 .body(Mono.just(BeerRepositoryTest.getTestBeer()), BeerDTO.class)
                 .exchange()
                 .expectStatus().isNoContent();
+    }
+
+    @Test
+    void createBeerBadData() {
+        Beer testBeer = BeerRepositoryTest.getTestBeer();
+        testBeer.setBeerName("");
+        webTestClient.post().uri(BeerController.BEER_PATH)
+                .body(Mono.just(testBeer), BeerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .expectStatus().isBadRequest();
     }
 
     @Test
